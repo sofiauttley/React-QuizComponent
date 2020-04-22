e053bb396eed9ba5a16a9576dc891fa3eb66405d
Fia <sofiauttley@yahoo.com>
2020-04-22 15:32:09 +0100

    Second attempt at Module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion';
-import QuizEnd from '/.QuizEnd';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json')
 
