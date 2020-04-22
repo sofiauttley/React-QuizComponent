0e07e8c577256a0da81c3fdd7631e771b67aa7df
Fia <sofiauttley@yahoo.com>
2020-04-22 15:22:58 +0100

    Finish of Module 3

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class QuizQuestionButton extends React.Component {
+    render(){
+        return(
+            <li>
+                <button>
+                {button_text}
+                </button>
+            </li>
+        )
+    }
+}
+
+
+export default QuizQuestionButton
