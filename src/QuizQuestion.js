73af57d96c59e8b5e93186f947c91d48542bb8fa
Fia <sofiauttley@yahoo.com>
2020-04-22 15:05:55 +0100

    Startpoint of Module 2

---
+++ src/QuizQuestion.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class QuizQuestion extends React.Component {
+    render() {
+        <main>
+            <section>
+                <p></p>
+            </section>
+            <section className="buttons">
+                <ul>
+                </ul>
+            </section>
+        </main>
+
+    }
+}
+
+export default QuizQuestion
