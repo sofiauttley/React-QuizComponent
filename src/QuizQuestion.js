b62bed4263c471e7fdd4878278d52e210c459d9d
Fia <sofiauttley@yahoo.com>
2020-04-23 13:02:56 +0100

    Second attempt at module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,9 +11,9 @@ class QuizQuestion extends React.Component {
                     </p>
                 </section>
                 <section className="buttons">
-                    <ul>
+                    <ul this.props.quiz_question.answer_options.map(answer_option, index)>
                         <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}
-                        quiz_question.map
+                        quiz_question.answer_options.map
                         />
                     </ul>
                 </section>
